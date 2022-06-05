cff614a2f8d7ee1b4ec4d16cd5c18493c8bb7734
Aforina <43246362+Aforina@users.noreply.github.com>
2022-06-05 18:20:00 +0200

    personal resume updated, svg icons included and main page advances

--- portfolio/components/common/skill.js
+++ portfolio/components/common/skill.js
@@ -2,9 +2,17 @@ import Image from 'next/image'
 
 const Skill = ({ image, description }) => {
   return (
-    <div>
-      {image}
-      {description}
+    <div className='flex flex-col space-y-2'>
+      <div className='flex justify-center'>
+        <Image
+          src={image}
+          alt='skill icon'
+          width={60}
+          height={60}
+          objectFit='contain'
+        />
+      </div>
+      <p className='flex justify-center'>{description}</p>
     </div>
   )
 }
--- portfolio/pages/projects.js
+++ portfolio/pages/projects.js
@@ -1,10 +1,12 @@
 import Nav from '../components/Nav'
+import Footer from '../components/Footer'
 
 const Projects = () => {
   return (
     <div id='projects'>
       <Nav />
       <h1>Esto es pagina proyectos</h1>
+      <Footer />
     </div>
   )
 }
